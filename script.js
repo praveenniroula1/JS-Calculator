4bb68d3e8110138dfae790dc93039a8990efe985
praveenniroula1 <101380394+praveenniroula1@users.noreply.github.com>
2022-12-22 09:45:28 +1100

    Functioning

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 const buttons = document.querySelectorAll(".btn-area>div");
-console.log(buttons);
 
 const display = document.querySelector(".display");
-console.log(display);
 
 const buttonArray = Array.from(buttons);
-console.log(buttonArray);
 
 const screen = (str) => {
   display.innerText = str || "0.00";
